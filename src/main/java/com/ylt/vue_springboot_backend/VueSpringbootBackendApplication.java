7411fa48fedb86d3138348066b0ef83b3b747bd4
YL-Ting <godcoolx@gmail.com>
2022-08-04 22:57:34 +0800

    init : create a new springboot project fix : mybatis connecting problem #20220804001

---
+++ src/main/java/com/ylt/vue_springboot_backend/VueSpringbootBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ylt.vue_springboot_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VueSpringbootBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VueSpringbootBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ylt/vue_springboot_backend/VueSpringbootBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ylt.vue_springboot_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VueSpringbootBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
